ea4dfd7c6afaf707d225cbca48813a6aa98ab2ab
Pujari Santhosh Babu <santhosh.b@geekyants.com>
2025-01-03 18:15:48 +0530

    pan gesture handler animation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Animation1 from "./animations/animation1";
+import Animation2 from "./animations/animation2";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Animation1 />
+      <Animation2 />
       <StatusBar style="auto" />
     </View>
   );
---
+++ animations/animation2.tsx
@@ -0,0 +1,98 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
+import { GestureHandlerRootView } from "react-native-gesture-handler"; // Import GestureHandlerRootView
+
+const SIZE = 100.0;
+const CIRCLE_RADIUS = SIZE * 2;
+
+type ContextType = {
+  translateX: number;
+  translateY: number;
+};
+
+const Animation2 = () => {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const panGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    ContextType
+  >({
+    onStart: (event, context) => {
+      context.translateX = translateX.value;
+      context.translateY = translateY.value;
+    },
+    onActive: (event, context) => {
+      translateX.value = event.translationX + context.translateX;
+      translateY.value = event.translationY + context.translateY;
+    },
+    onEnd: (event) => {
+      const distance = Math.sqrt(translateX.value ** 2 + translateY.value ** 2);
+      if (distance <= CIRCLE_RADIUS + SIZE / 2) {
+        translateX.value = withSpring(0, { duration: 2000 });
+        translateY.value = withSpring(0, { duration: 2000 });
+      }
+    },
+  });
+
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+      ],
+    };
+  });
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <View style={styles.circle}>
+          <PanGestureHandler onGestureEvent={panGestureEvent}>
+            <Animated.View style={[styles.square, reanimatedStyle]}>
+              <Text style={{ color: "white" }}>Animation2</Text>
+            </Animated.View>
+          </PanGestureHandler>
+        </View>
+      </View>
+    </GestureHandlerRootView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  square: {
+    width: SIZE,
+    height: SIZE,
+    backgroundColor: "rgba(0,0,256,0.5)",
+    borderRadius: SIZE / 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  circle: {
+    width: CIRCLE_RADIUS * 2,
+    height: CIRCLE_RADIUS * 2,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: CIRCLE_RADIUS,
+    borderWidth: 5,
+    borderColor: "rgba(0, 0, 256, 0.5)",
+  },
+});
+
+export default Animation2;
